79ef495b6d5bf4846134cf826400d29e6024dbd0
ashwinweb2595 <ashwinweb2595@gmail.com>
2022-06-17 01:02:12 +0530

    change index

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vashitva Raj Website
         </a>
       </header>
     </div>
