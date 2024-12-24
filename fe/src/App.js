cc9b9a2d9eee6bd707dba1db630a78b179b815b9
Ashwin Sinha <ashwinweb2595@gmail.com>
2024-12-24 22:35:49 +0530

    updating files

--- fe/src/App.js
+++ fe/src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ textAlign: 'center' }}
         >
-          Learn React
+          <img src={'./assets/bgimage.jpg'} alt='hero' style={{ width: '50%', margin: '0 auto' }} />
         </a>
       </header>
     </div>
